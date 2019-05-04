0c548c5472184cda05ff6693973d5a15beeca16d
zhujialei <jerru96868@gmail.com>
2019-05-04 13:58:23 +0800

    变量引用

--- lambda/src/lambda/VarDemo.java
+++ lambda/src/lambda/VarDemo.java
@@ -11,6 +11,8 @@ public class VarDemo {
 
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
+		//在lambda中表达式可以访问list，但是list实际上编译器会帮我们加上final
+		//list = new ArrayList<>(); //这样就会编译错误，因为是final的不能修改
 		Consumer<String> consumer = s -> System.out.println(s + list);
 		consumer.accept("1211");
 	}
---
+++ zmydemo/src/main/java/com/lambda/TypeDemo.java
@@ -0,0 +1,13 @@
+package com.lambda;
+
+public class TypeDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+@FunctionalInterface
+interface IMath{
+    int add(int x,int y);
+}
